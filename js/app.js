de8c60441b5a83d0cc5bdc7798986ef89bf55b8e
Akhbrand <snzwvccmny@privaterelay.appleid.com>
2026-07-31 22:31:49 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -112,24 +112,6 @@ toast.classList.remove("show");
 
 }
 
-function showToast(message){
-
-const toast =
-document.getElementById("toast");
-
-if(!toast) return;
-
-toast.innerHTML = message;
-
-toast.classList.add("show");
-
-setTimeout(()=>{
-
-toast.classList.remove("show");
-
-},2200);
-
-}
 /* =========================
    OPEN PRODUCT
 ========================= */
